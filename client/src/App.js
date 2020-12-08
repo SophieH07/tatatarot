6188c787419535e603cbdcace9be6b8dc22cd483
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-12-08 18:54:54 +0100

    create buttons on front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import './App.css';
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Button } from "reactstrap";
+import Axios from "axios";
+import NavMenu from "./components/NavMenu";
+import welcome from "./pictures/welcomeontarottrans.png";
+import logo from "./pictures/tarotlogotrans.png";
 
 function App() {
-  
   const [cards, setCards] = useState([]);
 
   useEffect(() => {
-    Axios.get('http://localhost:3001/cards').then((res) => {
+    Axios.get("http://localhost:3001/cards").then((res) => {
       setCards(res.data);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-    <div className="App">
-      <h1>TATATAROT</h1>
-      {cards.map((value)=> {
-        return <p>{value.name} | {value.upward} | {value.downward}</p>  
-      })}
+    <div>
+      <NavMenu />
+      <div className="main">
+        <img src={welcome} className="welcome" />
+        <p>
+          <img src={logo} className="logo" />
+        </p>
+        <p>
+          <button type="button" className="front-page-button">
+            What is tarot?
+          </button>
+          <button type="button" className="front-page-button">
+            See all cards
+          </button>
+          <button type="button" className="front-page-button">
+            Pick a card
+          </button>
+        </p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* {cards.map((value) => {
+  return (
+    <p>
+      {value.name} | {value.upward} | {value.downward}
+    </p>
+  );
+})} */
+}
--- client/src/components/NavMenu.js
+++ client/src/components/NavMenu.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+
 
 function NavMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <>
-    
-    </>
+      <Navbar color="dark" dark expand="md" sticky="top">
+        <NavbarBrand href="/">TATATAROT</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/description">
+                What is TAROT?
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/cards">See Cards</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/generator">Pick your fortune</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
   );
 }
-
 
 export default NavMenu;
