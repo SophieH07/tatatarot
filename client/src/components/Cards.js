bf09e6b80db609d34c808fa2e69150ab878454df
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-12-08 23:24:28 +0100

    delete axios

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,15 +1,7 @@
 import React from "react";
-import Axios from "axios";
 import { cardPicDic } from "./GenerateCard.js";
 
 function Cards() {
-  const [cards, setCards] = useState([]);
-
-  useEffect(() => {
-    Axios.get("http://localhost:3001/cards").then((res) => {
-      setCards(res.data);
-    });
-  }, []);
 
   return (
     <div>
@@ -25,8 +17,3 @@ function Cards() {
 }
 
 export default Cards;
-
-// {cards.map((value) => {
-//     return <p>{value.name}</p>
-// })}
-// <img src={chariot} />
