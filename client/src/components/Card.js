1639787806f1b9481ed0e84b568be943e2c473b9
SophieH07 <53736936+SophieH07@users.noreply.github.com>
2020-12-30 12:51:37 +0100

    add css to card

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -6,11 +6,11 @@ export class Card extends Component {
         const card = this.props.location.state;
         return (
             <div className="cards">
-                <h2 className="name">{card.name}</h2>
+                <h1 className="name">{card.name}</h1>
                 <img src={card.pic} className="picture" alt=""/>
-                <h3 className="name">UPWARD MEANING</h3>
+                <h3 className="name">upward meaning</h3>
                 <h4 className="card-description">{card.upward}</h4>
-                <h3 className="name">DOWNWARD MEANING</h3>
+                <h3 className="name">downward meaning</h3>
                 <h4 className="card-description">{card.downward}</h4>
             </div>
         )
